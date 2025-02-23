90a2bfb4d32b438e49e0a09b0efb118182963ce4
MarceloZilotti <zilotti@yahoo.com>
2025-02-23 01:41:45 -0300

    Update Age Calculator in Decades.py
    Acrescentado os anos, além da década

--- Age Calculator in Decades.py
+++ Age Calculator in Decades.py
@@ -1,3 +1,4 @@
 age = int(input ("Sua idade é?\n"))
 decadas = age//10
-print ("Você tem" +" "+ str(decadas) +" " +"décadas de idade.")
+anos = age % 10
+print ("Você tem" +" " + str(decadas) + " " + "décadas" + "e " + str(anos) + " " + "anos" +  " " + "de idade.")
